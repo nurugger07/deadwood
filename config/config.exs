be629546c664475b0f035b12b266fdc756ce4bee
Johnny Winn <johnny@elixirfountain.com>
2017-04-01 17:13:42 -0400

    Show example leader_node config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,9 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
+
 use Mix.Config
+
+config :coyote, leader_node: [:"web@<sname>", :"api@<sname>", :"server@<sname>"]
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
